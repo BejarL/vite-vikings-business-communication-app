0356cce092d9cf825a51ce9f6a57a503e486574b
Bejar L <143468066+BejarL@users.noreply.github.com>
2024-02-05 14:15:37 -0800

    Changed the colour scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import Login from "./components/Login"
-
-import Dashboard from "./components/Dashboard"
-function App() {
-  return (
-    <>
-      {/* <Login /> */}
-      <Dashboard />
-
 import SignUp from "./components/SignUp"
+import Dashboard from "./components/Dashboard"
+
 function App() {
   return (
     <>
       <Login />
       <SignUp />
+      <Dashboard />
     </>
   )
 }
