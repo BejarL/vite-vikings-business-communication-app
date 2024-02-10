caea3baa3f572a0f7ab4349295e9eb8f4fb0f9ef
BenjaminMaring <benjammaring@gmail.com>
2024-02-09 17:44:05 -0700

    got picture uploading finished, may need security rules setup still. need to add functionality to change users display name and delete account still
    Co-authored-by: jaydgm <jaydgm@users.noreply.github.com>
    Co-authored-by: JustinValencia <JustinValencia@users.noreply.github.com>

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 import { getDatabase } from "firebase/database"
+import { getStorage } from "firebase/storage"
 // import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBPqZ24MX5WzGq3RCadi7BGnkk2tLC-xbw",
   authDomain: "emanate-demo.firebaseapp.com",
   projectId: "emanate-demo",
@@ -17,4 +18,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const database = getDatabase(app)
+export const storage = getStorage(app);
 // const analytics = getAnalytics(app);  
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,53 @@
 import React from 'react';
 import EditUserNameModal from './EditUserNameModal'
 import DeleteAccountModal from './DeleteAccoutModal';
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth, storage } from '../../FirebaseConfig.js'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import './Profile.css'
 
 export default function Profile({ userID }) {
     const [currentName, setCurrentName] = React.useState("xlnvis")
-    const [img, setImg] = React.useState("https://source.unsplash.com/random")
-    console.log(img);
+    const [img, setImg] = React.useState("")
+    const [userId, setUserId] = React.useState("");
 
+    onAuthStateChanged(auth, (currentUser) => {
+        setUserId(currentUser.uid);
+    });
+    
+    React.useEffect(() => {
+        if (userId) {
+            const imageRef = ref(storage, `profilePicture/${userId}/profilePic`)
+            const url = getDownloadURL(imageRef)
+                .then((res) => setImg(res))
+                .catch(() => console.log("initial load failed or doesnt exist"))
+        }
+    }, [userId])
+    
+    //is used to handle any file that gets uploaded.
+    // 1st - checks to see if a file has been uploaded, if its undefined then do nothing
+    // 2nd- creates an image ref unique to the signed in user, the img ref file path is profilePicture/${userId}/profilePic in storage
+    // 3rd - once the image ref is made, then upload the file to that img ref in storage, and if it failes then console log 'upload failed'
+    // 4th - if the upload is successfull, then try to get the URL of the img in storage, otherwise log an error about getting the url
+    // 5th - when it successfully gets the img url, then set state to update what the user sees
     const handleFileUpload = (e) => {
-        setImg(() => URL.createObjectURL(e.target.files[0]));
+        // const userId = auth.currentUser.uid
+        const file = e.target.files[0];
+        if (file !== undefined) {
+            const imageRef = ref(storage, `profilePicture/${userId}/profilePic`)
+            uploadBytes(imageRef, file)
+                .then(() => {
+                    const url = getDownloadURL(imageRef)
+                        .then((res) => setImg(res))
+                        .catch(() => console.log("error getting url"))
+                })
+                .catch(() => console.log("upload failed"));
+        }
     }
 
-
+    const deleteProfile = () => {
+        
+    }
 
     return (
         <div className='profile--wrapper'>
@@ -27,7 +62,7 @@ export default function Profile({ userID }) {
                         onChange={handleFileUpload}
                     />
                     <label htmlFor="fileInput" className="custom-button">Choose a File</label>
-                    <img className="profile--img" alt="profile picture" src={img}></img>
+                    <img className="profile--img" src={img}></img>
                 </div>
                 <div className="profile--settings-wrapper">
                     <div className="profile--user-wrapper">
