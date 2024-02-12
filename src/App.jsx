5b56125948fa73f2fe2ed9b7953034ce3d11c741
sjendar <your_email@example.com>
2024-02-12 13:19:39 -0800

    Added text box code

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Login from "./components/Login"
 import Dashboard from "./components/Dashboard"
 import SignUp from "./components/SignUp"
 
+
 function App() {
   return (
-    <>
+<>
+    <Dashboard/>
       <Login />
       <SignUp />
     </>
