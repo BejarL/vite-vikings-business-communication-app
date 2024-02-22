08a05cf5eb4152f60e3b98424e41ad385294ea83
Justin Valencia <justinvalencia2023@gmail.com>
2024-02-22 13:18:37 -0800

    pushing the tailwind css additions to the sidebar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -48,7 +48,7 @@ function Dashboard() {
       console.log(obj);
 
       return (
-        <div key={obj.channelId}>{obj.channelName}</div>
+        <div key={obj.channelId} className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300">{obj.channelName}</div>
         )
     });
     } 
@@ -95,7 +95,7 @@ function Dashboard() {
 			<span className="ml-2 text-sm font-bold">Emanate</span>
 		</a>
 		<div className="w-full px-2">
-			<div className="flex flex-col items-center w-full mt-3 border-t border-blue-900">
+			<div className="flex flex-col items-center w-full mt-3 border-t border-b border-blue-900">
 				<Link className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" to="/profile">
           <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -105,13 +105,15 @@ function Dashboard() {
 					<NewChannel />
 			</div>
 		</div>
+    <div className="flex flex-col items-center w-full max-h-[100%] overflow-y-auto mt-3  pe">
+      <div className="">
+        {channelElems}
+      </div>
+    </div>
 		<Link className="flex items-center justify-center w-full h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300" to="/" onClick={handleLogout}>
 		<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="3em" viewBox="0 0 24 24"><path fill="currentColor" d="M5 3h6a3 3 0 0 1 3 3v4h-1V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-4h1v4a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3m3 9h11.25L16 8.75l.66-.75l4.5 4.5l-4.5 4.5l-.66-.75L19.25 13H8z"/></svg>
 			<span className="ml-2 text-sm font-medium">Log Out</span>
 		</Link>
-    <div className="flex flex-col items-center w-full mt-3 border-t border-blue-900">
-        
-    </div>
 	</div>
       </nav>
 
