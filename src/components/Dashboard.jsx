7d483953a713ad050f9323e69408461200228939
sjendar <your_email@example.com>
2024-02-23 11:03:56 -0800

    add function to textbox

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -111,8 +111,94 @@ const handleLogout = async () => {
                     </div>
                 </div>
             </div>
+
+               
+            import React, { useState} from 'react';
+            function ChannelComponent() {
+                const [channelTitle, setChannelTitle] = useState("vite Vikings Channel"); //holding channel title
+                
+                const updateChannelTitle = () => {
+                    setChannelTitle("Vite Vikings Channel"); //function update (updating channel title)
+                };
+            return (
             <div className="dashboard--textbox">
             </div>
-        </div>
+            <div className='title'>
+            <div className='title-wrapper'>
+            
+            <h1 className='channel-title'>{ChannelTitle}</h1>
+            );
+            }
+            
+            export default channelComponent;
+            
+            export default function Dashboard() {
+
+            const handleSettingsClick = () => {
+            {
+                console.log ('settings button clicked')
+            }
+            
+                return (
+            <button className='settings-button' onClick={handleSettingsClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg></button>
+            
+                 </div>
+                 </div>
+                );
+                //incoming message
+                function Message({ senderName, messageContent, messageDate }) {
+                    return (
+                
+
+                 <div className="message-box">
+                 <div className="sender-info">
+                 <svg className="icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 15 15"><path fill="currentColor" d="M4.5 11.5H4a.5.5 0 0 0 .5.5zm6 0v.5a.5.5 0 0 0 .5-.5zm-6 .5h6v-1h-6zm6.5-.5v-2h-1v2zm-7-2v2h1v-2zM7.5 6A3.5 3.5 0 0 0 4 9.5h1A2.5 2.5 0 0 1 7.5 7zM11 9.5A3.5 3.5 0 0 0 7.5 6v1A2.5 2.5 0 0 1 10 9.5zm3 2c0 .245-.114.52-.406.816c-.294.299-.745.59-1.341.846c-1.191.51-2.871.838-4.753.838v1c1.984 0 3.804-.344 5.147-.92c.67-.287 1.245-.642 1.659-1.061c.416-.422.694-.936.694-1.519zM7.5 14c-1.882 0-3.562-.328-4.753-.838c-.596-.256-1.047-.547-1.341-.846C1.114 12.02 1 11.747 1 11.5H0c0 .583.278 1.097.694 1.519c.414.42.989.774 1.66 1.062C3.695 14.656 5.515 15 7.5 15zM1 11.5c0-.242.11-.513.394-.805c.286-.294.725-.582 1.306-.837l-.4-.916c-.656.287-1.218.64-1.622 1.056C.27 10.416 0 10.925 0 11.5zm11.3-1.642c.581.255 1.02.543 1.305.837c.284.292.395.563.395.805h1c0-.575-.27-1.084-.678-1.502c-.404-.416-.966-.769-1.622-1.056zM7.5 4A1.5 1.5 0 0 1 6 2.5H5A2.5 2.5 0 0 0 7.5 5zM9 2.5A1.5 1.5 0 0 1 7.5 4v1A2.5 2.5 0 0 0 10 2.5zM7.5 1A1.5 1.5 0 0 1 9 2.5h1A2.5 2.5 0 0 0 7.5 0zm0-1A2.5 2.5 0 0 0 5 2.5h1A1.5 1.5 0 0 1 7.5 1z"></path></svg>
+                 <span className="sender-name">{senderName}</span>
+                 <span className="message-date">{messageDate}</span>
+                 
+                    <div className="message">
+                    <p className='message content'>{messageContent}</p>
+                    </div>
+                    export default function MessageList({ messages }) {
+                        return (
+                            <div>
+                            {messages.map((message,index) => <Message key={index}
+                            senderName={message.senderName}
+                            messageContent={message.messageContent}
+                            messageDate={message.messageDate}/> )}
+                            </div>
+                        );
+                    }
+                        
+                    <button className='delete-button' onClick={handleDeleteClick}> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg> 
+
+                    </button>
+                     </div>
+                 
+                 </div>
+                 </div>
+                 const [replyText, setReplyText] = useState('');
+                 const handleChange = (event) => {
+                    setReplyText (event.target.value);
+                 };
+                 //reply functionality
+                 const handleReply = () => {
+                    console.log('Reply submitted:', replyText); 
+                    //clear the text area after submitting
+                    setReplyText('');
+                 };
+                 return (
+                 
+                  <div className="reply.box">
+                    <textarea className="reply-textarea" placeholder="type your reply here..." rows={3} cols={50} value={replyText} onChange={handleChange}/>
+                    <button className="reply-button" onClick={handleReply} disabled={replyText.trim}>Reply</button>
+                  </div>
+                  </div>
+                  );
+            }
+            
+
     )
 }
+    
