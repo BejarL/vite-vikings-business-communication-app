54c3ad3084ce8cd69e2e1e2b3c8f0f33fe54f9e4
BenjaminMaring <benjammaring@gmail.com>
2024-02-26 17:59:45 -0700

    got it to sort the messages automatically
    Co-authored-by: jaydgm <jaydgm@users.noreply.github.com>
    Co-authored-by: Lisset Bejar <BejarL@users.noreply.github.com>
    Co-authored-by: sjhendar <sjendar@users.noreply.github.com>
    Co-authored-by: CiegeX <CiegeX@users.noreply.github.com>

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,18 +1,23 @@
 import { useState, useEffect } from 'react';
-import { onSnapshot, addDoc, collection, Timestamp } from "firebase/firestore";
+import { onSnapshot, addDoc, collection, query, getDocs, orderBy } from "firebase/firestore";
 import { auth, db } from "../../FirebaseConfig";
 
 export default function TextArea({ channel }) {
     const [messages, setMessages] = useState([]);
     const [messageField, setMessageField] = useState("");
+    const [loading, setloading] = useState(false);
 
     const chat = collection(db, `Chats/${channel.channelId}`, "messages");
 
     useEffect(() => {
         if (channel) {
-            
-            const unsubscribe = onSnapshot(chat, (snapshot) => {
-                const messagesData = snapshot.docs.map((doc) => doc.data());
+            const messageQuery = query(chat, orderBy("createdAt"));
+
+            const unsubscribe = onSnapshot(messageQuery, (snapshot) => {
+                let messagesData = [];
+                snapshot.forEach((doc) => {
+                    messagesData.push(doc.data())
+                });
                 setMessages(messagesData);
             })
             return unsubscribe;
@@ -32,44 +37,78 @@ export default function TextArea({ channel }) {
             userId: auth.currentUser.uid
         }
 
-        console.log(newMessage);
-        // const messagesRef = collection(newChannelRef, "messages");
-        // await addDoc(messagesRef, {
-        //   userId: "System",
-        //   body: "This is the start of the Channel!",
-        //   createdAt: new Date(),
-        //   authorProfilePic: "",
-        //   messageId: crypto.randomUUID()
-        // })
-
-
-
         await addDoc(chat, newMessage);
+    }
+
+    const convertDate = (date) => {
+
+        const options = {
+            // or 'short' or 'narrow'
+            year: 'numeric',
+            month: 'long', // or 'short' or 'narrow'
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          };
+        
+        const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+        return formattedDate.replace('at', '');
+        
     }
 
     const updateMessage = () => {
         
     }
 
-    const deleteDoc = () => {
+    const deleteMsg = () => {
         
     }
 
     const messagesElems = messages.map(msg => {
-        return <p key={msg.messageId}>{msg.body}</p>
+
+
+        let timeStamp = convertDate(msg.createdAt)
+
+        return (
+            <div key={msg.messageId}>
+                <div>
+                    <div>
+                        <img src={msg.profilePic} alt="profile picture"></img>
+                        <p>{msg.userId}</p>
+                    </div>
+                    <p>{timeStamp}</p>
+                </div>
+                <p>{msg.body}</p>
+            </div>
+        )
     })
 
-  return (
-    <div className="relative dashboard--textbox w-[100%] flex flex-col">
-      <div className="w-100 h-8 mt-3 border-b border-blue-900 m-1">
-        <p className="ml-7 text-base">{channel.channelName}</p>
-      </div>
-      <div className="text-area overflow-y-auto">{messagesElems}</div>
+    
 
-            <div className="absolute bottom-0 w-[100%] ">
-                <input className="w-[100%]" value={messageField} placeholder="Type your message here" onChange={handleMessageField}/>
-                <button onClick={sendMessage}>Send</button>
-            </div>
-      </div>
+    return (
+        <div className="relative dashboard--textbox w-[100%] flex flex-col">
+        <div className="w-100 h-8 mt-3 border-b border-blue-900 m-1">
+            <p className="ml-7 text-base">{channel.channelName}</p>
+        </div>
+        <div className="text-area overflow-y-auto">{loading ? "loading" : messagesElems}</div>
+
+                <div className="absolute bottom-0 w-[100%] ">
+                    <input className="w-[100%]" value={messageField} placeholder="Type your message here" onChange={handleMessageField}/>
+                    <button onClick={sendMessage}>Send</button>
+                </div>
+        </div>
   )
 }
+
+
+
+{/* <div key={msg.messageId}>
+                <div>
+                    <div>
+                        <img src={msg.profilePic} alt="profile picture"></img>
+                        <p>{displayName}</p>
+                    </div>
+                    <p>{msg.createdAt}</p>
+                </div>
+                <p>{msg.body}</p>
+            </div> */}
