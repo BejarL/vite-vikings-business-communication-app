72396b072686dd6895ff3e3fb36a76a0ab48f326
BenjaminMaring <benjammaring@gmail.com>
2024-03-01 15:47:47 -0700

    add functionality to add users after a channel has been created. Changed how we are loading user data so its with a onSnapshot. also changed how the messages were being loaded, to where it pushes the messages into the array, and when it renders them it reverses them
    Co-authored-by: JustinValencia <JustinValencia@users.noreply.github.com>

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -50,7 +50,7 @@ export default function TextArea({ channel }) {
           data.createdAt = data.createdAt?.toDate
             ? data.createdAt.toDate()
             : data.createdAt;
-          messagesData.unshift(data); // Prepends the new message to maintain order after sorting by 'createdAt' desc
+          messagesData.push(data); // Prepends the new message to maintain order after sorting by 'createdAt' desc
         });
         setMessages(messagesData);
         setLoading(false);
@@ -60,30 +60,32 @@ export default function TextArea({ channel }) {
     }
   }, [channel, messagesLimit]);
 
+  //
   useEffect(() => {
     if (!loading && channel) {
-      getUsers();
+
+      const membersQuery = query(
+        membersRef
+      );
+
+      const unsubscribe = onSnapshot(membersQuery, async (snapshot) => {
+        let usersData = [];
+        const dataArray = snapshot.docs
+        for (let i in dataArray) {
+          const userSnap = await getDoc(doc(db, "users", dataArray[i].data().userId))
+          if (userSnap.exists()) {
+              usersData.push({uid: userSnap.data().uid, 
+                              displayName: userSnap.data().displayName,  
+                              profilePic: userSnap.data().authorProfilePic
+                            })
+          }
+        }
+        setUsers(usersData);
+      });
+      return () => unsubscribe();
     }
   }, [loading])
 
-  const getUsers = async() => {
-    const usersData = [];
-    //get the collection of users in the channel
-    const usersQuery = query(membersRef)
-    const querySnapshot = await getDocs(usersQuery);
-    const dataArray = querySnapshot.docs;
-    
-    for (let i in dataArray) {
-      const userSnap = await getDoc(doc(db, "users", dataArray[i].data().userId))
-      
-      if (userSnap.exists()) {
-          usersData.push({uid: userSnap.data().uid, displayName: userSnap.data().displayName})
-      }
-      
-    }
-    console.log(usersData)
-    setUsers(usersData);
-  }
 
   const handleMessageField = (e) => {
     setMessageField(e.target.value);
@@ -231,7 +233,7 @@ export default function TextArea({ channel }) {
           {channel.channelName}
         </span>
         { channel ?
-          <ChannelSettingsModal channel={channel} users={users}/>
+          <ChannelSettingsModal channel={channel} users={users} />
           : null
         }
       </h1>
@@ -249,16 +251,16 @@ export default function TextArea({ channel }) {
           )}
           <div>
             {loading ? 
-<div class="flex justify-center items-center h-screen">
-    <div role="status">
-        <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-        </svg>
-        <span class="sr-only">Loading...</span>
-    </div>
-</div>
-              : messagesElems
+              <div className="flex justify-center items-center h-screen">
+                  <div role="status">
+                      <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                      </svg>
+                      <span className="sr-only">Loading...</span>
+                  </div>
+              </div>
+              : messagesElems.reverse()
               }
             <div ref={messagesEndRef} />
           </div>
@@ -275,15 +277,15 @@ export default function TextArea({ channel }) {
         />
 
         <button type="button" 
-          class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           onClick={sendMessage}
           disabled={loading || !messageField.trim()}
           aria-label="Send message"        
         >
-          <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+          <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
           </svg>
-          <span class="sr-only">Icon description</span>
+          <span className="sr-only">Icon description</span>
         </button>
       </div>
     </div>
